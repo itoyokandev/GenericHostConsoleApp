a3497d7253f05c99d885e64893a401d186b849a8
Yohei Ito <cts.yohei.ito@gmail.com>
2022-01-04 18:11:02 +0900

    プロジェクト ファイルを追加します。

---
+++ GenericHostApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using AppLifetime.Example;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<ExampleHostedService>();
+            });
+}
